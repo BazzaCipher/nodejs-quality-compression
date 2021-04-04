d39afc42ed870b4d292d95fc12c323258a0bb4bc
platy11 <mightymaxmax@gmail.com>
2021-04-04 18:44:40 +1000

    Fix bug, other improvements

--- index.js
+++ index.js
@@ -2,24 +2,29 @@ const firebase = require('firebase-admin')
 const minify = require('imagemin')
 const express = require('express')
 const auth = require('./token')
+const multer = require('multer')
 
 const app = express()
+const upload = multer({
+    limits: {
+        // Limit images to 10MB to prevent denial of service
+        fieldSize: 10 * 1024 * 1024
+    }
+})
 
+// Needed to get the client's IP on Heroku
 app.enable('trust proxy')
 
-app.get('*', (_, res) => {res.end()})
+app.get('/', (_, res) => {
+    res.end("Success")
+})
 
-app.get('/', (_, res) => {res.end("Success")})
-
-app.post('/', auth.verifyToken, (req, res) => {
-    // Do stuff to the data
-    if (req.headers['content-type'] !== 'multipart/form-data') {
-        return res.end()
-    }
-
+app.post('/', auth.verifyToken, upload.single('image'), (req, res) => {
+    // Uploaded file is req.file
+    // https://github.com/expressjs/multer#file-information
     console.log(req.statusCode)
     console.log(req.body)
-    res.end('success')
+    res.end('Success')
 })
 
 // Heroku sets the PORT environment variable
--- token.js
+++ token.js
@@ -1,18 +1,23 @@
 const firebase = require('firebase-admin')
 const { writeFileSync } = require('fs')
 
+// The Firebase Admin SDK requires that credentials are stored in a JSON file.
+// The app is deployed to Heroku without changes from the git repo, so we can
+// only store private data in env vars. Therefore at startup, we need to copy
+// the data from the env var to a file.
 writeFileSync("/app/creds.json", process.env.GOOGLE_CREDENTIALS, 'utf8')
 
 firebase.initializeApp({
-    credential: admin.credential.applicationDefault(),
+    credential: firebase.credential.applicationDefault(),
     databaseURL: 'https://hackiethon-food-photo-journal.firebaseio.com'
 });
 
 // Firebase token verification function
 // Usage: app.get('/', verifyToken, (req, res) => {...})
+// Adds the property firebaseUserId to req, as this is used as the DB key
 function verifyToken(req, res, next) {
     let token = req.get('X-Firebase-Token')
-    admin.auth().verifyIdToken(token).then(decoded => {
+    firebase.auth().verifyIdToken(token).then(decoded => {
         req.firebaseUserId = decoded.uid
         next()
     }).catch(() => {
