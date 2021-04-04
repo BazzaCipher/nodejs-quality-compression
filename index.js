8f72e8bb4491b9cb6d0dca6958e13406dc47fd0c
BazzaCipher <33623276+BazzaCipher@users.noreply.github.com>
2021-04-04 22:49:27 +1000

    small commit please check

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const firebase = require('firebase-admin')
-const minify = require('imagemin')
+const jpegP = require('imagemin-jpegtran')
+const pngP = require('imagemin-pngquant')
 const express = require('express')
-const auth = require('./token')
 const multer = require('multer')
+const fs = require('fs')
+const auth = require('./token')
+const imagemin = require('imagemin')
+const shorthash = require('shorthash')
 
 const app = express()
 const upload = multer({
@@ -11,6 +15,9 @@ const upload = multer({
         fieldSize: 10 * 1024 * 1024
     }
 })
+
+// Create temporary folder to hold files in conversion and compression states
+fs.mkdirSync('./temp')
 
 // Needed to get the client's IP on Heroku
 app.enable('trust proxy')
@@ -24,7 +31,23 @@ app.post('/', auth.verifyToken, upload.single('image'), (req, res) => {
     // https://github.com/expressjs/multer#file-information
     console.log(req.statusCode)
     console.log(req.body)
-    res.end('Success')
+
+    const ext = req.file.mimetype.split('/').pop()
+    const fn = shorthash.unique(req.headers['x-firebase-token'])
+    const fna = shorthash.unique(req.headers['x-firebase-token'] + ext)
+    const fp = `./temp/${fn}.${ext}`
+    const fpa = `./temp/${fna}.${ext}`
+    
+    fs.writeFileSync(fp, req.file.buffer, /*{ encoding: ext }*/)
+
+    imagemin(fp, fpa, {
+        use: [
+            pngP(),
+            jpegP()
+        ]
+    })
+
+    res.end()
 })
 
 // Heroku sets the PORT environment variable
