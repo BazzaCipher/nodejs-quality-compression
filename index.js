51607d5072fe62bd2d99f4f2b52891d911e1cbee
BazzaCipher <33623276+BazzaCipher@users.noreply.github.com>
2021-04-04 14:45:41 +1000

    Express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const firebase = require('firebase-admin')
+// const firebase = require('firebase-admin')
+const minify = require('imagemin')
+const express = require('express')
+
+const app = express()
+
+app.get('/', (_, res) => {res.end("Success")})
+
+app.post('/')
+
+app.listen(3000)
